1a500ce1c5cf66156238917ab0f1defdeceaa0fb
Luyanda <luyanda.s.zuma@gmail.com>
2025-06-28 13:54:21 +0200

    Updated index.js
    Added searchCity function and updateWeatherInfo function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ function displaySearchInput(event) {
   let searchInput = document.querySelector("#search-input");
   let cityName = document.querySelector("h1");
   cityName.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", displaySearchInput);
+
+function searchCity(city) {
+  let apiKey = "a3f9ff86f4t0ab9cbe395o06803b6fc9";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
+function updateWeatherInfo(response) {
+console.log(response.data);
+}
