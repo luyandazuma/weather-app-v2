7a83e06265b2316bce1b799d3227477018de177c
Luyanda <luyanda.s.zuma@gmail.com>
2025-07-01 16:26:57 +0200

    Updated index.html, style.css and index.js
    Added ids and removed city name, and weather details in index.html. Changed weather details style in styles.css. Updated apiUrl, and updateWeatherInfo function.

--- src/index.js
+++ src/index.js
@@ -12,10 +12,29 @@ searchElement.addEventListener("submit", displaySearchInput);
 function searchCity(city) {
   let apiKey = "a3f9ff86f4t0ab9cbe395o06803b6fc9";
   let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(updateWeatherInfo);
 }
 
 function updateWeatherInfo(response) {
-console.log(response.data);
+  let currentTemperature = document.querySelector("#current-temp-value");
+  let temperature = response.data.temperature.current;
+  currentTemperature.innerHTML = Math.round(temperature);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  let conditionElement = document.querySelector("#weather-condition");
+  conditionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#weather-humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity + "%";
+
+  let windElement = document.querySelector("#weather-wind");
+  windElement.innerHTML = response.data.wind.speed + "km/h";
+
+  let feelsLikeElement = document.querySelector("#feels-like");
+  feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like) + "°C";
 }
+
+searchCity("Durban");
